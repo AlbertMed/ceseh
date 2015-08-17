d9b9f146a121c12904183ebe6d32e12b50e02faa
Albert Med <albert91.me.d@gmail.com>
2015-08-17 01:45:30 -0500

    rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,16 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+Route::get('productos/carrito/items/{token}/={usuario}','CarritoController@items');
+
+Route::get('productos/{categoria}/datos/{valor}','ProductoController@datos');
+
+Route::get('productos/{categoria}', 'ProductoController@listarProductos');
+
+Route::get('carrito', 'CarritoController@index');
+
+Route::get('addItemCarrito/{code}/{token}', 'CarritoController@store');
+
+Route::get('deleteItem/{user}/{id}/{token}','CarritoController@delete');
