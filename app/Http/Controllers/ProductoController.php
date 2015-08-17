240aa60a47fcdb376d4d4386c079f0c0a42387fd
Albert Med <albert91.me.d@gmail.com>
2015-08-16 23:40:19 -0500

    ok
    ok

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Articulos;
+use DB;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(){
+
+        $categoria = DB::table('producto')->distinct()->select('SubMarca')->get();
+
+
+        return view('home')->with('categorias',$categoria);
+    }
+
+     /**
+      * Display list of products
+      * 
+      * @return products
+      */
+     public function listarProductos($categoria){
+        $articulos = Articulos::where('SubMarca', '=',$categoria)->paginate(12);
+
+        return view('producto.listar2')->with('datos',$articulos);
+    }
+
+    /**
+     * Display detall of a product selected
+     * 
+     * @return product
+     */ 
+    public function datos($categoria,$valor){
+        $wsdl = "http://187.188.85.203:8036/Sample.asmx?WSDL";
+        $client = new \nusoap_client($wsdl, true);
+        $idLogin = $client->call('Login');
+        $ID = $idLogin['LoginResult']."";
+        /*
+          funcion para sacar el detalle de un producto 
+         */
+        $ItemList = $client->call('GetDetalle',array('SID' => $ID , 'producto' => $valor));
+        $productos = (string)$ItemList['GetDetalleResult'];
+        $datos = utf8_encode($productos);
+        $BOM = new \SimpleXMLElement($datos);
+        //datos a mostrar en la interfaz
+        $itemName = $BOM->BO->Items->row->ItemName;
+        $itemCode = $BOM->BO->Items->row->ItemCode;
+
+        if ($BOM->BO->Items_Prices->row[0]->Currency=="MXP"){
+            $numero = ($BOM->BO->Items_Prices->row[1]->Price); 
+            $numero = number_format($numero,4,'.',''); 
+            $precio = $numero; 
+        }else{
+            $number = (float)$BOM->BO->Items_Prices->row[1]->Price; 
+            $number = number_format($number,4,'.','');  
+            $numero = ($number)*16.269951;    
+            $numero = number_format($numero,4,'.','');  
+            $precio = $numero; 
+        }  
+
+        $stock = $BOM->BO->Items->row->QuantityOnStock*1;
+
+
+
+        return view('detalle_producto')->with(compact('itemName','itemCode','precio','stock'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
